ffba9b29cd65cfef50b618f7139accb17db8cb7e
webcog <wajahatmurtaza144@gmail.com>
2023-12-04 22:48:07 +0500

    up

---
+++ loop.py
@@ -0,0 +1,30 @@
+# i = 1
+# while i < 6:
+#   print(i)
+#   if (i==3):
+#     break
+#   i = i + 1
+#   continue
+#   break
+# i = 0
+# while i < 6:
+#     i = i + 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+# x = ["hello","abc","cyx","zyx"]
+x = [546,546,5798,423,455]
+# x.remove("hi")
+# x.pop(1)
+# x.clear()
+x.sort()
+print(x)
+# y = [1,2,3]
+# z = x+y
+# x.extend(y)
+# x.insert(1,"Insert Value")
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(1, "orange")
+# print(thislist)
