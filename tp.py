092963402ca0924f29413070cc3b0ab1b1f60b4b
webcog <wajahatmurtaza144@gmail.com>
2023-12-05 21:29:09 +0500

    update

---
+++ tp.py
@@ -0,0 +1,53 @@
+import random
+while True:
+    x = random.randint(1,10)
+    y = input(">> ")
+    z = int(y)
+    if z == x:
+        print("You Won")
+
+    else:
+        print("You Lose")
+        print("Computer Guess:" ,x)
+
+
+# x = ["abc","xyz","sjkfhksf",32132]
+# print(type(x))
+# y = ("xyz","abc",12,12)
+# z = list(y)
+# z[1]="up"
+# x = tuple(z)
+# print(x)
+
+# x = ["abcc","xyz"]
+# y = [1,2]
+# z = x+y
+# print(z)
+
+
+# s = {"abc","zyc",1,2,2}
+# print(s)
+# s = ["Mustang","Wizit","Honda","Civic"]
+# car = {
+#     'a':1,
+#     'b':2
+# }
+# x = {
+#     'car':"Ford",
+#     'food':"fastFood",
+#     "age":18,
+#     "model":"Mustang"
+#     }
+# x.update({"year":2023})
+# z = xalues()
+# z = x.ke.vys()
+# print(x)
+# z = x.get("car")
+# print(z)
+# x = [] list 
+# x = () tuples 
+# x = {} sets
+# x = {} dict 
+
+
+# 4-dight 
