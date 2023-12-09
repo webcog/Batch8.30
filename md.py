195fb5428c447dbf313229ad78af83bd99fa288b
webcog <wajahatmurtaza144@gmail.com>
2023-12-09 21:49:08 +0500

    up

---
+++ md.py
@@ -0,0 +1,29 @@
+# import random
+# # password 
+# # Ab$8 > 8
+# # Abc&*.0;
+# lower = "qwertyuiopasdfghjklzxcvbnm"
+# upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
+# symbol = "~`!@#$%^&*()_-+={}[]|/.':;"
+# number = "0123456789"
+
+# all = lower + upper + symbol + number
+
+
+# length = input(">> ")
+# x = int(length)
+# password = "".join(random.sample(all, x))
+# print("Genrated Password:",password)
+
+# random 
+# os sys 
+# django 
+import random
+import pyautogui 
+
+# print(x)
+# pip uninstall pyautogui 
+# sample
+# def abc():
+#     print("func")
+
