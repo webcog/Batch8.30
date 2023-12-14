8782d4a2141cacc04e0b4ce010c4291a88ca5d78
webcog <wajahatmurtaza144@gmail.com>
2023-12-14 20:58:30 +0500

    change

---
+++ class.py
@@ -0,0 +1,37 @@
+# class MyClass:
+#     x = 5
+#     y = 6
+
+# c = MyClass()
+# print(c.x)
+# print(c.y)
+
+# [1,2,3] list  index,change,dulp
+# (1,2,3) tuple index, unchange, duplic
+# {1,2,3} sets unindex,unch,dup not allow
+# {"key":valye} dict , index , changble, duplic not allow
+ 
+# x = {"car":"ford", "food":"kuchb"}
+# print(x)
+# 
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 2:
+#     break
+#   i = i + 1
+
+# i = 0
+# while i < 6:
+#   i = i + 1
+#   if i == 3:
+#     continue
+#   print(i)
+
+fruits = ["apple", "banana", "cherry","pineapple"]
+# lenght = 4 
+# a = 0
+#  a < 4
+# a = a+1
+for a in fruits:
+  print(a)
